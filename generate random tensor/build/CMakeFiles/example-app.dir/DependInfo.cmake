09aab3fb9a6601261db743382c46e90fcbc26541
Praveenkanna <jpraveenkanna@gmail.com>
2020-11-16 19:58:21 +0530

    format the folders

---
+++ generate random tensor/build/CMakeFiles/example-app.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/praveenkanna/Local disk/Workspace/CPP-ML_Implementation/generate random tensor/generateRandomTensor.cpp" "/media/praveenkanna/Local disk/Workspace/CPP-ML_Implementation/generate random tensor/build/CMakeFiles/example-app.dir/generateRandomTensor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/media/praveenkanna/Local disk/Download/libtorch/include"
+  "/media/praveenkanna/Local disk/Download/libtorch/include/torch/csrc/api/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
