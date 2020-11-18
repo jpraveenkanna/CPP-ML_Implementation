d09a99ebe63549561438095baf24b71133ed1c25
Praveenkanna <jpraveenkanna@gmail.com>
2020-11-18 16:48:59 +0530

    add code to run torchscript from CPP

---
+++ TorchScript load model/build/CMakeFiles/example-app.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/praveenkanna/HardDiskE/Workspace/CPP-ML_Implementation/TorchScript load model/app.cpp" "/media/praveenkanna/HardDiskE/Workspace/CPP-ML_Implementation/TorchScript load model/build/CMakeFiles/example-app.dir/app.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/media/praveenkanna/HardDiskE/libtorch/include"
+  "/media/praveenkanna/HardDiskE/libtorch/include/torch/csrc/api/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
